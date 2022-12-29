7e444912db012740486c651daa82536368869c64
jensi2809 <jensimaniya99@gmail.com>
2022-12-28 22:59:01 -0500

    first commit

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -3,7 +3,6 @@ import About from "../../Components/About";
 import Banner from "../../Components/Banner";
 import Services from "../../Components/Services";
 import "./style.css";
-
 const Home = () => {
   return (
     <div className="home-wrapper">
